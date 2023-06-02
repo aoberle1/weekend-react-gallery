717868e5ffaf276fecd2138496adc6c6de1d8aec
Adam Oberle <oberle.adam@gmail.com>
2023-06-01 21:44:45 -0500

    added 5 cat related images - added object in array in gallery.data.js for each pic

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/paulie.jpeg', description: 'Agent orange in his element', likes: 1 },
+    { id: 3, path: 'images/saucey.jpeg', description: 'The big one', likes: 2 },
+    { id: 4, path: 'images/sleepy.jpeg', description: 'Someone sure is sleepy', likes: 3 },
+    { id: 5, path: 'images/tuna.jpeg', description: 'The old man himself', likes: 4 },
+    { id: 6, path: 'images/two_cats.jpeg', description: 'Two is better than one', likes: 5}
 ];
 
 module.exports = galleryItems;
